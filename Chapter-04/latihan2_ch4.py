01ce7d167ee8f6a03b3d862bd29bf41dce4e85bc
danangkurniawan110 <89757116+danangkurniawan110@users.noreply.github.com>
2021-10-06 16:44:10 +0700

    Delete Chapter-04 directory

--- Chapter-04/latihan2_ch4.py
+++
@@ -1,6 +0,0 @@
-#Menghitung jumlah liter bensin untuk menempuh jarak
-print('Menghitung banyak liter bensin yang diperlukan:')
-print('_______________________________________________')
-Jarak=float(input('Jarak (km):'))
-liter=float(input('Jarak yang dapat ditempuh per liter:'))
-print('Jumlah liter bensin yang diperlukan:',Jarak/liter,'liter')
